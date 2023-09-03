6a46f4c7cdff453fcec6c0f1af3c9b56376556bc
tainamamederibeiro@gmail.com <tainamamederibeiro@gmail.com>
2023-09-03 19:09:26 +0100

    First Commands print and input

--- class2.py
+++ class2.py
@@ -1,2 +1,30 @@
-print("teste")
+#Funções dir() e help() para descobrir todos os parâmetros.
+
+#Comando print()
+
+print("Olá mundo!")
+print(6+5)
+print("6","5")
+print("6"+"5")
+
+#Variáveis(caixa) são espaços de memória, utilizado para "guardar" uma determinada informação
+#Comando de atribuição: (=) recebe / type() mostra o tipo de dado da variável
+x=5
+print(x)
+y=6
+print("O resultado da soma é: ", x+y)
+
+#Comando input()
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+print(nome,"tem", idade,"anos")
+
+x = int(input("Digite o valor de x: "))
+y = int(input("Digite o valor de y: "))
+print("Resultado da soma: ", x+y)
+
+
+
+
+
 
