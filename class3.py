fcc834b3b12ffe036ace7b88348b40862b76dcbc
tainamamederibeiro@gmail.com <tainamamederibeiro@gmail.com>
2023-09-04 13:29:30 +0100

    Fundamentals Python

---
+++ class3.py
@@ -0,0 +1,39 @@
+#Identificadores: Nome de variáveis, funções, classes e módulos
+    #Pode ser combinações de letras maiúsculas e minúsculas, dígito e undescore (_)
+    #Não pode iniciar com dígito
+    #Não pode utilizar dígitos
+        #Ex. Variável: data_de_nascimento (...pode assumir qualquer um dos valores em um conjunto de valores/ espaço na memória/tem endereço)
+        #Ex. Classe: DataDeNacimento
+        #Ex. Método: test_test():
+
+#Tipos de dados em Python:
+    #Primitivos:
+        #strg, int, float, complex, bool
+    #Outras estruturas de Dados:
+        #Listas (list), Tuplas (tuple), Dicionário (dic) e outros.
+
+#Constante: algo que não muda.
+    #O nome de variáveis devem ser em MAIÚSCULA.
+        #Ex. PI = 3.1415 / area do PI * (raio*raio)
+
+#Apresentação dos Tipos Primitivos em Python:
+#Teste no Terminal:
+
+>>>10
+>>>type(10)
+# <class 'int'>
+>>>6.0
+>>>type(6.0)
+#<class 'float'>
+>>>"test_test"
+>>>type("test_test")
+#<class 'str'>
+>>>5==2
+>>>False
+>>> type(False)
+#<class 'bool'>
+
+
+
+
+
