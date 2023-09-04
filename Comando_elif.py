549e55282d7cea447ff54ea5a6c0f7d96f87d7c5
tainamamederibeiro@gmail.com <tainamamederibeiro@gmail.com>
2023-09-04 16:44:27 +0100

    Learn command elif

---
+++ Comando_elif.py
@@ -0,0 +1,22 @@
+idade = int(input("Qual sua idade: "))
+if idade >= 18:
+    print("Parabéns! Você já é maior de idade")
+
+else:
+    print("Espere mais um pouco! Logo será maior de idade. Bye")
+
+#Another ex.:
+
+nota1 = float(input("Digite sua nota1: "))
+nota2 = float(input("Digite sua nota2: "))
+media = (nota1 + nota2)/2
+if media >=5:
+    print("APROVADO! Sua nota final é: ", media)
+else:
+    print("REPROVADO! Sua nota final é: ", media)
+
+
+
+
+
+
