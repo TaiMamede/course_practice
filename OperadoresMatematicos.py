8402433a4c615c9e776d2eda404795024b2fbd18
tainamamederibeiro@gmail.com <tainamamederibeiro@gmail.com>
2023-09-04 14:17:50 +0100

    Operadores Matematicos

---
+++ OperadoresMatematicos.py
@@ -0,0 +1,13 @@
+#Operadores Matematicos
+'''
+
++ (adição): 5+3=8
+- (subtração): 5-3=2
+* (multiplicação): 5*3=15
+/ (divisão): 5/3=1.666666...
+% (módulo): 5%3=2
+** (exponenciação): 5**2 = 25
+// (divisão inteira): 5//2 = 2
+
+'''
+
